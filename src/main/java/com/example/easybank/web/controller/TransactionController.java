afb03169131b0c0058072aee057ecb7478277ae3
Arguera123 <00090822@uca.edu.sv>
2025-06-15 17:59:16 -0600

    fix(transaction): change request from transactions

--- src/main/java/com/example/easybank/web/controller/TransactionController.java
+++ src/main/java/com/example/easybank/web/controller/TransactionController.java
@@ -1,16 +1,18 @@
 package com.example.easybank.web.controller;
 
 import com.example.easybank.application.dto.request.TransactionRequestDTO;
+import com.example.easybank.application.dto.response.TransactionResponseDTO;
 import com.example.easybank.application.service.TransactionService;
 import com.example.easybank.util.GenericResponse;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Transaction;
 import org.hibernate.metamodel.internal.AbstractPojoInstantiator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.List;
 
 import static com.example.easybank.util.Constant.*;
 
@@ -26,6 +28,16 @@ public class TransactionController {
         return GenericResponse.builder()
                 .message("Transaction successfully")
                 .status(HttpStatus.CREATED)
+                .build().buildResponse();
+    }
+
+    @GetMapping(FIND_OWN)
+    public ResponseEntity<GenericResponse> findOwnTransaction() throws Exception {
+        List<TransactionResponseDTO> transactions = transactionService.findMyOwnTransactions();
+        return GenericResponse.builder()
+                .data(transactions)
+                .message("Transactions found")
+                .status(HttpStatus.OK)
                 .build().buildResponse();
     }
 }
