02df6aed292432cb83a99d0bf2b64209db7907e9
valeor0224 <00206022@uca.edu.sv>
2025-06-18 05:50:32 -0600

    add(userlist): controller

--- src/main/java/com/example/easybank/web/controller/UserListController.java
+++ src/main/java/com/example/easybank/web/controller/UserListController.java
@@ -1,9 +1,42 @@
 package com.example.easybank.web.controller;
 
-public class UserListController {
 
+import com.example.easybank.application.dto.response.UserResponseDTO;
+import com.example.easybank.application.service.UserListService;
+import com.example.easybank.util.GenericResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.easybank.util.Constant.*;
+
+@RequestMapping(API+ADMIN)
+@RequiredArgsConstructor
+@RestController
+public class UserListController {
+    private final UserListService userListService;
+
+
+    @GetMapping(USER_LIST)
+    public ResponseEntity<GenericResponse> getAllUsers() throws Exception {
+        List<UserResponseDTO> users = userListService.findAllUsers();
+
+        return GenericResponse.builder()
+                .data(users)
+                .message("Users found")
+                .status(HttpStatus.OK)
+                .build().buildResponse();
+
+    }
 
 }
+
+
+
+
 
 
 
